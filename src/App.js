dafa08f2f104373227f2578731891d3fb2a561ab
swright <swright@overstock.com>
2018-10-04 13:22:40 -0600

    changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          I am so flippin awesome!
+          I am so flippin awesome! (Feature branch change)
         </p>
       </div>
     );
